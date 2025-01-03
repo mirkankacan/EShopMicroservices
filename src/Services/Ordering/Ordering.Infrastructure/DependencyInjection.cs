45cc1231c2c6b169aea27ea86b16b1cacdab0089
Mirkan Kaçan <kacanmirkan@gmail.com>
2025-01-03 16:32:55 +0300

    Domain modelling for ordering.

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MssqlConnection");
+
+            //services.AddDbContext<OrderContext>(options =>
+            //    options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
